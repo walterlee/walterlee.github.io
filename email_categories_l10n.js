a12ae1a8f0dbae72b83b6881f3ddfcb4aba2d492
Walter Lee <walter@leanplum.com>
2017-04-19 15:29:01 -0700

    changing from JSON to JS embed

---
+++ email_categories_l10n.js
@@ -0,0 +1,174 @@
+$(function() {
+  String.toLocaleString({
+    "de": {
+      "Account Insights": "Benutzerkonto-Einblicke",
+      "Personalized Suggestions": "Personalisierte Vorschläge",
+      "Marketing and Newsletters": "Marketing und Newsletter",
+      "Personal Account Information": "Persönliche Benutzerkontoinformationen",
+      "Welcome": "Willkommen",
+      "Announcements and New Features": "Ankündigungen und neue Funktionen",
+      "Research and Surveys": "Forschung und Umfragen",
+      "Account Activity": "Benutzerkontoaktivitäten"
+    },
+    "el": {
+      "Account Insights": "Στοιχεία Λογαρισμού",
+      "Personalized Suggestions": "Προσωπικές Προτάσεις",
+      "Marketing and Newsletters": "Μάρκετινγκ και Ενημερωτικά",
+      "Personal Account Information": "Πληροφορίες Προσωπικού Λογαριασμού",
+      "Welcome": "Καλώς Ήρθατε",
+      "Announcements and New Features": "Ανακοινώσεις και Νέα Χαρακτηριστικά",
+      "Research and Surveys": "Έρευνα και Δημοσκοπήσεις",
+      "Account Activity": "Δραστηριότητα Λογαριασμού"
+    },
+    "es": {
+      "Account Insights": "Datos sobre tu cuenta",
+      "Personalized Suggestions": "Sugerencias personalizadas",
+      "Marketing and Newsletters": "Marketing y Newsletters",
+      "Personal Account Information": "Información personal de la cuenta",
+      "Welcome": "Bienvenida",
+      "Announcements and New Features": "Anuncios y nuevas funcionalidades",
+      "Research and Surveys": "Investigación y encuestas",
+      "Account Activity": "Actividad de la cuenta"
+    },
+    "fi": {
+      "Account Insights": "Faktoja tilistäsi",
+      "Personalized Suggestions": "Yksilöidyt ehdotukset",
+      "Marketing and Newsletters": "Markkinointi ja uutiskirjeet",
+      "Personal Account Information": "Oman tilin tiedot",
+      "Welcome": "Tervetuloa",
+      "Announcements and New Features": "Ilmoitukset ja uudet toiminnot",
+      "Research and Surveys": "Tutkimus ja kyselyt",
+      "Account Activity": "Tilin tapahtumat"
+    },
+    "fr": {
+      "Account Insights": "Données du Compte",
+      "Personalized Suggestions": "Suggestions Personnalisées",
+      "Marketing and Newsletters": "Marketing et Newsletters",
+      "Personal Account Information": "Informations Compte Personnel",
+      "Welcome": "Bienvenue",
+      "Announcements and New Features": "Communiqués et Nouvelles Fonctions",
+      "Research and Surveys": "Recherche et Sondages",
+      "Account Activity": "Activité du Compte"
+    },
+    "fr-CA": {
+      "Account Insights": "Données du Compte",
+      "Personalized Suggestions": "Suggestions Personnalisées",
+      "Marketing and Newsletters": "Marketing et Newsletters",
+      "Personal Account Information": "Informations Compte Personnel",
+      "Welcome": "Bienvenue",
+      "Announcements and New Features": "Communiqués et Nouvelles Fonctions",
+      "Research and Surveys": "Recherche et Sondages",
+      "Account Activity": "Activité du Compte"
+    },
+    "hu": {
+      "Account Insights": "Fiókadatok",
+      "Personalized Suggestions": "Személyre szabott javaslatok",
+      "Marketing and Newsletters": "Marketing és hírlevelek",
+      "Personal Account Information": "Személyes fiókadatok",
+      "Welcome": "Üdvözlet",
+      "Announcements and New Features": "Közlemények és új funkciók",
+      "Research and Surveys": "Kutatások és felmérések",
+      "Account Activity": "Fióktevékenységek"
+    },
+    "it": {
+      "Account Insights": "Dettagli account",
+      "Personalized Suggestions": "Suggerimenti personalizzati",
+      "Marketing and Newsletters": "Marketing e newsletter",
+      "Personal Account Information": "Informazioni personali sull'account",
+      "Welcome": "Benvenuto",
+      "Announcements and New Features": "Annunci e nuove funzionalità",
+      "Research and Surveys": "Ricerche e sondaggi",
+      "Account Activity": "Attività dell'account"
+    },
+    "ko": {
+      "Account Insights": "계정 내역",
+      "Personalized Suggestions": "개별 제안",
+      "Marketing and Newsletters": "마케팅 및 뉴스레터",
+      "Personal Account Information": "계정 정보",
+      "Welcome": "환영합니다",
+      "Announcements and New Features": "공지사항 및 새로운 기능",
+      "Research and Surveys": "연구 및 설문조사",
+      "Account Activity": "계정 활동내역"
+    },
+    "nb": {
+      "Account Insights": "Kontoinnsikt",
+      "Personalized Suggestions": "Personlige forespørsler",
+      "Marketing and Newsletters": "Markedsføring og nyhetsbrev",
+      "Personal Account Information": "Personlig kontoinformasjon",
+      "Welcome": "Velkommen",
+      "Announcements and New Features": "Kunngjøringer og nye funksjoner",
+      "Research and Surveys": "Forskning og undersøkelser",
+      "Account Activity": "Kontoaktivitet"
+    },
+    "nl": {
+      "Account Insights": "Account Inzichten",
+      "Personalized Suggestions": "Gepersonaliseerde Suggesties",
+      "Marketing and Newsletters": "Marketing en Nieuwsbrieven",
+      "Personal Account Information": "Persoonlijke Accountgegevens",
+      "Welcome": "Welkom",
+      "Announcements and New Features": "Aankondigingen en Nieuwe Functies",
+      "Research and Surveys": "Onderzoek en Enquêtes",
+      "Account Activity": "Account Activiteit"
+    },
+    "pt": {
+      "Account Insights": "Informações sobre a conta",
+      "Personalized Suggestions": "Sugestões Personalizadas",
+      "Marketing and Newsletters": "Marketing e Newsletters",
+      "Personal Account Information": "Informações da Conta Pessoal",
+      "Welcome": "Bem-vindo",
+      "Announcements and New Features": "Anúncios e Novos Recursos",
+      "Research and Surveys": "Pesquisas e Inquéritos",
+      "Account Activity": "Atividade da Conta"
+    },
+    "pt-BR": {
+      "Account Insights": "Informações sobre a conta",
+      "Personalized Suggestions": "Sugestões Personalizadas",
+      "Marketing and Newsletters": "Marketing e Newsletters",
+      "Personal Account Information": "Informações da Conta Pessoal",
+      "Welcome": "Bem-vindo",
+      "Announcements and New Features": "Anúncios e Novos Recursos",
+      "Research and Surveys": "Pesquisas e Inquéritos",
+      "Account Activity": "Atividade da Conta"
+    },
+    "ru": {
+      "Account Insights": "Детали аккаунта",
+      "Personalized Suggestions": "Персональные предложения",
+      "Marketing and Newsletters": "Маркетинг и новости",
+      "Personal Account Information": "Персональная информация аккаунта",
+      "Welcome": "Добро пожаловать",
+      "Announcements and New Features": "Анонсы и обновления",
+      "Research and Surveys": "Исследования и опросы",
+      "Account Activity": "Активность аккаунта"
+    },
+    "sv": {
+      "Account Insights": "Kontoöversikt",
+      "Personalized Suggestions": "Specialanpassade Förslag",
+      "Marketing and Newsletters": "Marknadsföring och Nyhetsbrev",
+      "Personal Account Information": "Personlig Kontoinformation",
+      "Welcome": "Välkommen",
+      "Announcements and New Features": "Nyheter och Nya Funktioner",
+      "Research and Surveys": "Sökningar och Undersökningar",
+      "Account Activity": "Kontoaktivitet"
+    },
+    "tr": {
+      "Account Insights": "Hesap İçgörüleri",
+      "Personalized Suggestions": "Kişiye Özel Öneriler",
+      "Marketing and Newsletters": "Tanıtım ve Bültenler",
+      "Personal Account Information": "Kişisel Hesap Bilgileri",
+      "Welcome": "Hoşgeldin",
+      "Announcements and New Features": "Duyuru ve Yeni Özellikler",
+      "Research and Surveys": "Araştırma ve Anketler",
+      "Account Activity": "Hesap Hareketleri"
+    },
+    "vi": {
+      "Account Insights": "Thông tin Tài khoản",
+      "Personalized Suggestions": "Đề xuất Cá nhân hóa",
+      "Marketing and Newsletters": "Tiếp thị và Thư thông báo",
+      "Personal Account Information": "Thông tin Tài khoản Cá nhân",
+      "Welcome": "Xin chào mừng",
+      "Announcements and New Features": "Thông báo và Tính năng mới",
+      "Research and Surveys": "Nghiên cứu và Khảo sát",
+      "Account Activity": "Hoạt động Tài khoản"
+    }
+  });
+});
